2b4c598a919e54899d66a4472ff58dfa62b12c12
Caique <caiquepinheiro69@gmail.com>
2025-10-23 22:43:29 -0300

    First commit

---
+++ Models/Bolo.cs
@@ -0,0 +1,9 @@
+namespace BlazorTall.Models
+{
+    public class Bolo
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public string Imagem { get; set; } = string.Empty;
+    }
+}
